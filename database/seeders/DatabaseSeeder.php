8747a694b1d3ddaf8358e67e2d4c5ac0ee5fe98a
Daniel Mendez <danielmenc@outlook.com>
2023-04-26 20:12:51 -0600

    #comment add admin user in database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Cupboard\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,9 +18,11 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        \App\Models\User::factory()->create([
-            'name' => 'WebunderAdmin',
+        User::factory()->create([
+            'username' => 'admin',
             'email' => 'admin@webunderdevelopment.com',
+            'is_admin' => true,
+            'password' => bcrypt('dinofili23')
         ]);
     }
 }
