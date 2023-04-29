857cc971e28fb33c9eb533bea3580860a20ae4c8
Daniel Mendez <danielmenc@outlook.com>
2023-04-29 00:43:05 -0600

    #comment improve file structure

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Cupboard\{AuthController, CommentController, PostController, ReactionController, UserController };
+use App\Http\Controllers\Cupboard\Api\{ AuthController, CommentController, PostController, ReactionController, UserController };
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
