15cfad38088fbf7ef4dd253effeb61cb2dad78de
Daniel Mendez <danielmenc@outlook.com>
2023-08-31 21:09:58 -0600

    #comment clean reaction resource

--- app/Http/Resources/Cupboard/Reaction.php
+++ app/Http/Resources/Cupboard/Reaction.php
@@ -19,10 +19,7 @@ class Reaction extends JsonResource
     {
         return [
             'id'         => $this->hasAttribute('uuid'),
-            // 'user_id'    => new User($this->whenLoaded('user')),
-            // 'user_id'    => $this->hasAttribute('user_id'),
             'user'       => new User($this->user),
-            // 'post_id'    => $this->hasAttribute('post_id'),
             'post'       => new Post($this->post),
             'reaction'   => $this->hasAttribute('reaction'),
             'created_at' => $this->when($this->created_at, $this->created_at ? $this->created_at->toDateTimeString() : null),
