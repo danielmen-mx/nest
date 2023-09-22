f2c792f22635b1842df025ea0aef3a1f7b206ff4
Daniel Mendez <danielmenc@outlook.com>
2023-09-21 21:07:40 -0600

    #comment create suite test of UserController methods

--- tests/Feature/App/Http/Controllers/Cupboard/UserControllerTest.php
+++ tests/Feature/App/Http/Controllers/Cupboard/UserControllerTest.php
@@ -2,16 +2,53 @@
 
 namespace Tests\Feature\App\Http\Controllers\Cupboard;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Cupboard\User;
 use Tests\TestCase;
+use Illuminate\Support\Str;
 
 # vendor/bin/phpunit tests/Feature/App/Http/Controllers/Cupboard/UserControllerTest.php
 class UserControllerTest extends TestCase
 {
-    /** @test */
-    public function store_new_user_successfully()
+    public function setUp(): void
     {
-        $response = $this->requestResource('POST', 'users', []);
-        dd($response);
+        parent::setUp();
+    }
+
+    /** @test */
+    function get_user_lists_successfully()
+    {
+        $response = $this->requestResource('GET', 'users', []);
+        $allUsers = User::get()->count();
+
+        $response->assertSuccessful();
+        $this->assertTrue(count($response->getData()->data) == $allUsers);
+    }
+
+    /** @test */
+    function update_user_data_success()
+    {
+        $user = User::factory()->create();
+        $payload = [
+            "id" => $user->uuid,
+            "email" => $user->username.now()->timestamp."@webunderdevelopment.com",
+            "first_name" => "Test" . now()->timestamp,
+            "last_name" => "Example" . now()->timestamp,
+            "username" => $user->username."-".now()->timestamp,
+            "language" => $user->language == 'es' ? 'en' : 'es',
+            "password" => "test-example-update"
+        ];
+
+        $response = $this->requestResource('PUT', "users/$user->uuid", $payload);
+        $response->assertSuccessful();
+        $this->assertDatabaseHas("users", [
+            'uuid' => $user->uuid,
+            "email" => $payload['email'],
+            "first_name" => $payload['first_name'],
+            "last_name" => $payload['last_name'],
+            "username" => $payload['username'],
+            "language" => $payload['language'],
+        ]);
+
+        $this->assertTrue(!password_verify($user->password, $payload['password']));
     }
 }
