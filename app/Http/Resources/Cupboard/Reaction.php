9b99fd65483c7592be4fd1e390f2c05aa366f91e
Daniel Mendez <danielmenc@outlook.com>
2023-10-09 00:24:35 -0600

    #comment improve the way we get the review && model resource from Reaction Resource

--- app/Http/Resources/Cupboard/Reaction.php
+++ app/Http/Resources/Cupboard/Reaction.php
@@ -3,11 +3,14 @@
 namespace App\Http\Resources\Cupboard;
 
 use App\Http\Resources\AdvancedResourceTrait;
+use App\Http\Resources\Cupboard\Post as CupboardPost;
+use App\Models\Cupboard\Post;
+use App\Models\Traits\GetModelTrait;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class Reaction extends JsonResource
 {
-    use AdvancedResourceTrait;
+    use AdvancedResourceTrait, GetModelTrait;
 
     /**
      * Transform the resource into an array.
@@ -17,12 +20,21 @@ class Reaction extends JsonResource
      */
     public function toArray($request)
     {
+        $model = $this->hasAttribute('model_type') == Post::class ? $this->post : null;
+
         return [
             'id'         => $this->hasAttribute('uuid'),
             'user'       => new User($this->user),
-            'post'       => new Post($this->post),
+            'model'      => $this->resource($this->hasAttribute('model_type'), $model),
+            'rating'     => new Review($this->model->review),
             'reaction'   => $this->hasAttribute('reaction'),
             'created_at' => $this->when($this->created_at, $this->created_at ? $this->created_at->toDateTimeString() : null),
         ];
+    }
+
+    private function resource($modelType, $model)
+    {
+        if ($modelType == Post::class) return new CupboardPost($model);
+        return null;
     }
 }
