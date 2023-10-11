21e933292bf55069b2b245b71dd6c882018a8700
Daniel Mendez <danielmenc@outlook.com>
2023-10-11 00:29:55 -0600

    #comment create products table

---
+++ database/migrations/Cupboard/2023_10_10_233710_create_products_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->uuid('uuid')->unique();
+            $table->string('name');
+            $table->decimal('price', 9, 2)->default(0.00);
+            $table->decimal('shipping_price', 9, 2)->default(0.00);
+            // quantity
+            // description
+            $table->string('assets')->nullable();
+            $table->unsignedBigInteger('review_id');
+            $table->foreign('review_id')->references('id')->on('reviews');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
