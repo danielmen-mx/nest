32744079d2f2b8904e02ac83b482fb8d171a8eb0
Daniel Mendez <danielmenc@outlook.com>
2023-10-13 00:03:48 -0600

    #comment complete ProductControllerTest TDD wip....

--- tests/Feature/App/Http/Controllers/Cupboard/ProductControllerTest.php
+++ tests/Feature/App/Http/Controllers/Cupboard/ProductControllerTest.php
@@ -2,9 +2,9 @@
 
 namespace Tests\Feature\App\Http\Controllers\Cupboard;
 
-use App\Models\Cupboard\User;
-use Tests\TestCase;
+use App\Models\Cupboard\{ Product, User };
 use Illuminate\Support\Str;
+use Tests\TestCase;
 
 # vendor/bin/phpunit tests/Feature/App/Http/Controllers/Cupboard/ProductControllerTest.php --filter={test-name}
 class ProductControllerTest extends TestCase
@@ -18,8 +18,25 @@ class ProductControllerTest extends TestCase
     }
 
     /** @test */
-    function get_index_of_products_success()
+    function get_index_of_products_from_admin_success()
     {
-        
+        $this->mockProducts(16);
+        $response = $this->requestResource('GET', 'products', ['per_page' => 15]);
+        $this->assertResponseSuccess($response);
+        $this->assertTrue(count($this->getData($response)) === 15);
+    }
+
+    /** @test */
+    function get_index_of_products_from_products_home_page_success()
+    {
+        $this->mockProducts(7);
+        $response = $this->requestResource('GET', 'products');
+        $this->assertResponseSuccess($response);
+        $this->assertTrue(count($this->getData($response)) === 6);
+    }
+
+    private function mockProducts($quantity = 1)
+    {
+        Product::factory($quantity)->withReview()->create();
     }
 }
