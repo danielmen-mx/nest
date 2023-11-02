e02af01648ecd9af42aaff46aead40d9fbf0a403
daniel <dmendez@webforcehq.com>
2023-11-01 23:20:24 -0600

    #comment create ProductSeeder file

---
+++ database/seeders/Cupboard/ProductSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders\Cupboard;
+
+use App\Models\Cupboard\Product;
+use Illuminate\Database\Seeder;
+
+// php artisan db:seed --class="Database\\Seeders\\Cupboard\\PostSeeder"
+class ProductSeeder extends Seeder
+{
+    public function run()
+    {
+        Product::factory(20)->withReview()->create();
+    }
+}
