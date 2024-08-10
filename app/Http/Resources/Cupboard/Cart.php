c14cf4319f725fbac724db979db6c16f23988abb
Daniel Mendez <danielmenc@outlook.com>
2024-08-10 07:43:54 -0600

    #comment add request resources files from cart module

---
+++ app/Http/Resources/Cupboard/Cart.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources\Cupboard;
+
+use App\Http\Resources\AdvancedResourceTrait;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Cart extends JsonResource
+{
+    use AdvancedResourceTrait;
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'         => $this->hasAttribute('uuid'),
+            'user_id'    => $this->user->uuid,
+            'user'       => new User($this->user),
+            'product_id' => $this->product->uuid,
+            'product'    => new Product($this->product),
+            'status'   => $this->hasAttribute('status'),
+            'quantity'   => $this->hasAttribute('quantity'),
+            'created_at' => $this->when($this->created_at, $this->created_at ? $this->created_at->toDateTimeString() : null),
+        ];
+    }
+}
