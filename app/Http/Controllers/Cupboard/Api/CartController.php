cf3f37ce8f04df3c8cd560b9663d379f423e1f1f
Daniel Mendez <danielmenc@outlook.com>
2024-08-10 07:42:23 -0600

    #comment complete crud methods of the CartController

--- app/Http/Controllers/Cupboard/Api/CartController.php
+++ app/Http/Controllers/Cupboard/Api/CartController.php
@@ -3,20 +3,36 @@
 namespace App\Http\Controllers\Cupboard\Api;
 
 use App\Http\Controllers\Cupboard\ApiController;
+use App\Http\Requests\Cupboard\Cart\{ Index, Store, Update};
+use App\Http\Resources\Cupboard\Cart as ResourceCart;
+use App\Http\Resources\Cupboard\CartCollection;
+use App\Models\Cupboard\{ Cart, Product, User };
+use App\Models\Traits\PaginationTrait;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 class CartController extends ApiController
 {
+    use PaginationTrait;
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request)
+    public function index(Index $request)
     {
-        try {    
+        try {
+            $userId = User::where('uuid', $request->user_id)->firstOrFail()->id;
+            $carts = Cart::query()
+                        ->where('user_id', $userId)
+                        ->where('status', $request->status)
+                        ->orderBy('created_at', 'asc')
+                        ->paginate($request->per_page??30);
 
+            $resource = $this->loadRequestResource($carts, $request->per_page);
+
+            return $this->responseWithPaginationResource(new CartCollection($carts), $resource, 'cart.index');
         } catch (\Exception $e) {
             return $this->responseWithError($e, 'cart.index');
         }
@@ -28,12 +44,25 @@ class CartController extends ApiController
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Store $request)
     {
         try {
-            
+            $data = $request->validated();
+            $user = User::where('uuid', $data['user_id'])->firstOrFail();
+            $product = Product::where('uuid', $data['product_id'])->firstOrFail();
+
+            $cart = Cart::create([
+              'user_id' => $user->id,
+              'product_id' => $product->id,
+              'status' => 'standby',
+              'quantity' => $data['quantity']
+            ]);
+
+            $cart->load(['user', 'product']);
+
+            return $this->responseWithData(new ResourceCart($cart), 'cart.store');
         } catch (\Exception $e) {
-          return $this->responseWithError($e, 'cart.store');
+            return $this->responseWithError($e, 'cart.store');
         }
     }
 
@@ -46,7 +75,11 @@ class CartController extends ApiController
     public function show($uuid)
     {
         try {
-            
+            $cart = Cart::query()
+                        ->where('uuid', $uuid)
+                        ->firstOrFail();
+
+            return $this->responseWithData(new ResourceCart($cart), 'cart.show');
         } catch (\Exception $e) {
           return $this->responseWithError($e, 'cart.show');
         }
@@ -59,10 +92,15 @@ class CartController extends ApiController
      * @param  \App\Models\Post  $post
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $uuid)
+    public function update(Update $request, $uuid)
     {
         try {
-            
+            $cart = Cart::where('uuid', $uuid)->firstOrFail();
+            $data = $request->validated();
+
+            $cart->update($data);
+
+            return $this->responseWithData(new ResourceCart($cart), 'cart.update');
         } catch (\Exception $e) {
           return $this->responseWithError($e, 'cart.update');
         }
@@ -77,9 +115,12 @@ class CartController extends ApiController
     public function destroy($uuid)
     {
         try {                        
-            
+            $cart = Cart::where('uuid', $uuid)->firstOrFail();
+            $cart->delete();
+
+            return $this->responseWithMessage('cart.delete');
         } catch (\Exception $e) {
-          return $this->responseWithError($e, 'cart.delete');
+            return $this->responseWithError($e, 'cart.delete');
         }
     }
 }
