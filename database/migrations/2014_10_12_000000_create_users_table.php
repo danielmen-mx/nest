6d6ed68c0c15e8572b820daa3c1327625f9e2dd5
daniel mendez <danielmenc@outlook.com>
2025-08-21 19:23:56 -0600

    #comment remove field from users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,7 +24,6 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->dateTime('deactivated_at')->nullable();
             $table->timestamps();
         });
     }
