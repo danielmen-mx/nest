aca160fbe6730957afaf707b9f0b3aa26ae23e5e
daniel mendez <danielmenc@outlook.com>
2025-08-22 23:19:12 -0600

    #commit delete a field from users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->string('last_name')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            
             $table->rememberToken();
             $table->timestamps();
         });
