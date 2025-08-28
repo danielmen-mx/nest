5faf69e060b302f7afbfe12f4cfed430f70a41bd
daniel mendez <danielmenc@outlook.com>
2025-08-27 23:42:35 -0600

    #commit fix User model path

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => App\Models\Cupboard\User::class,
         ],
 
         // 'users' => [
