86f4b68a9afee965f54648044f6494145de084ed
daniel mendez <danielmenc@outlook.com>
2025-08-27 23:42:07 -0600

    #commit implement new function to call success the function to reset password

--- app/Models/Cupboard/User.php
+++ app/Models/Cupboard/User.php
@@ -5,6 +5,7 @@ namespace App\Models\Cupboard;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 use App\Models\Traits\HasUuidTrait;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -14,6 +15,10 @@ use Laravel\Passport\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, SoftDeletes, Notifiable, HasUuidTrait;
+
+    protected $primaryKey = 'uuid';
+    public $incrementing = false;
+    protected $keyType = 'string';
 
     /**
      * The attributes that are mass assignable.
@@ -62,5 +67,24 @@ class User extends Authenticatable
     public function cart()
     {
         return $this->hasMany(Cart::class);
+    }
+
+    public function sendPasswordResetNotification($token)
+    {
+        $url = config('app.frontend_url').'/reset-password?token='.$token.'&email='.$this->email;
+
+        $this->notify(new class($url) extends ResetPassword {
+            private $url;
+            public function __construct($url) { $this->url = $url; }
+            public function via($notifiable) { return ['mail']; }
+            public function toMail($notifiable)
+            {
+                return (new \Illuminate\Notifications\Messages\MailMessage)
+                    ->subject('Reset Password Notification')
+                    ->line('You are receiving this email because we received a password reset request for your account.')
+                    ->action('Reset Password', $this->url)
+                    ->line('If you did not request a password reset, no further action is required.');
+            }
+        });
     }
 }
