0fb9b3337130e72c03a3690ac5d7061f78e88792
daniel mendez <danielmenc@outlook.com>
2025-09-07 13:38:48 -0600

    #commit add new user method to handle reset password

--- app/Models/Cupboard/User.php
+++ app/Models/Cupboard/User.php
@@ -5,12 +5,13 @@ namespace App\Models\Cupboard;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 use App\Models\Traits\HasUuidTrait;
-use Illuminate\Auth\Notifications\ResetPassword;
+// use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
+use App\Notifications\ResetPasswordNotification;
 
 class User extends Authenticatable
 {
@@ -58,10 +59,7 @@ class User extends Authenticatable
 
     public function getFullName()
     {
-        $firstName = $this->first_name;
-        $lastName = $this->last_name;
-
-        return $firstName.' '.$lastName;
+        return $this->first_name . ' ' . $this->last_name;
     }
 
     public function cart()
@@ -71,20 +69,6 @@ class User extends Authenticatable
 
     public function sendPasswordResetNotification($token)
     {
-        $url = config('app.frontend_url').'/reset-password?token='.$token.'&email='.$this->email;
-
-        $this->notify(new class($url) extends ResetPassword {
-            public $url;
-            public function __construct($url) { $this->url = $url; }
-            // public function via($notifiable) { return ['mail']; }
-            public function toMail($notifiable)
-            {
-                return (new \Illuminate\Notifications\Messages\MailMessage)
-                    ->subject('Reset Password Notification')
-                    ->line('You are receiving this email because we received a password reset request for your account.')
-                    ->action('Reset Password', $this->url)
-                    ->line('If you did not request a password reset, no further action is required.');
-            }
-        });
+        $this->notify(new ResetPasswordNotification($token));
     }
 }
