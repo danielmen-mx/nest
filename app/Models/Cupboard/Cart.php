326092764c4fda669077b4b2ed1f0288619851a9
daniel mendez <danielmenc@outlook.com>
2025-09-11 02:05:20 -0600

    #commit complement correctly the relations of cart with users and products models

--- app/Models/Cupboard/Cart.php
+++ app/Models/Cupboard/Cart.php
@@ -29,12 +29,12 @@ class Cart extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id', 'id');
     }
 
     public function product()
     {
-        return $this->belongsTo(Product::class);
+        return $this->belongsTo(Product::class, 'product_id', 'id');
     }
 
     public function standby()
